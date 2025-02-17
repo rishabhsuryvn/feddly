c3d6f0f03896057e1194fc6b12c4b6b112f978d7
Rishabh Suryavanshi <rishabhsuryavanshi@192.168.1.9>
2025-02-17 19:37:28 +0530

    final change

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,12 @@ export default function Header() {
     <header className="sticky inset-x-0 top-0 z-30 w-full transition-all bg-white/20 backdrop-blur-md">
       <div className="w-full max-w-screen-xl px-2.5 lg:px-20 relative mx-auto border-b">
         <div className="flex h-14 items-center justify-between">
-          <Image src="/logo.png" alt="logo" width={120} height={100} />
+          <Image
+            src="/logo.png 18-55-24-655.png"
+            alt="logo"
+            width={120}
+            height={100}
+          />
           <div className="flex justify-center">
             <SignedOut>
               <SignInButton>
